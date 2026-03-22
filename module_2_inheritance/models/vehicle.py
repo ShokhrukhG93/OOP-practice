15631e1d2be2ac9b9a32c41dce24bacad05536a0
ShokhrukhG93 <shokhrukhgaybullaev.93@gmail.com>
2026-03-22 19:57:25 +0500

    Add some methods to vehicle class

--- module_2_inheritance/models/vehicle.py
+++ module_2_inheritance/models/vehicle.py
@@ -4,7 +4,13 @@ class Vehicle:
         self.make = make
         self.model = model
 
+    def start(self):
+        print(f'{self.make} {self.model} is starting.')
+    
+    def __repr__(self):
+        return f'This vehicle is {self.make} {self.model}'
 
+    
 # Define the Car SubClass of Vehicle class
 class Car(Vehicle):
     def __init__(self, make, model, number_of_doors):
@@ -23,13 +29,16 @@ if __name__ == '__main__':
     # Create a Vehicle object
     vehicle = Vehicle('Generic Make', 'Generic Model')
     print(f'Make: {vehicle.make}, Model: {vehicle.model}')
+    print(vehicle)
 
     # Create a Car object
     car = Car('Honda', 'Civic', 4)
     print(f'''Make: {car.make}, Model: {car.model},
             Number of Doors: {car.number_of_doors}''')
+    print(car)
 
     # Create a Truck object
     truck = Truck('Ford', 'F-150', '1000 lbs')
     print(f'''Make: {truck.make}, Model: {truck.model},
             Cargo Capacity: {truck.cargo_capacity}''')
+    print(truck)
